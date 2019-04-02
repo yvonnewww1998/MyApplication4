288df385b51ab8f3140669192b6a9abbdd21727f
yvonnewww1998 <yvonnewww19981227@gmail.com>
2019-04-02 14:35:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.myapplication;
+
+import java.util.Calendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvDate, tvTime;
+    private Button btDate, btTime;
+    private int mYear, mMonth, mDay, mHour, mMinute;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvDate = (TextView) findViewById(R.id.tvDate);
+
+        btDate = (Button) findViewById(R.id.btDate);
+
+        btDate.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                showDatePickerDialog();
+            }
+        });
+    }
+
+    public void showDatePickerDialog() {
+        // 設定初始日期
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        // 跳出日期選擇器
+        DatePickerDialog dpd = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+                        // 完成選擇，顯示日期
+                        tvDate.setText(year + "-" + (monthOfYear + 1) + "-"
+                                + dayOfMonth);
+
+                    }
+                }, mYear, mMonth, mDay);
+        dpd.show();
+    }
+
+}
